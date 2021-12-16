50d2f2a48861fddc51ab87d44a4b7cb4243b325f
AzuraOne <meong@meong.com>
2021-12-17 02:34:55 +0700

    Penambahan DB + beberapa Menu

---
+++ BackendUAS/application/views/_partial/navbar.php
@@ -0,0 +1,6 @@
+<div class="container d-flex justify-content-between ">
+    <h3 class="fs-4 fw-bold text-center text-wrap text-white" style="width: 6rem;">Pencatatan Uang Mandiri</h3>
+
+    <a href="#" class="nav-link fs-2 text-white">ADMIN <i class="fas fa-key"></i></a>
+
+</div>
---
+++ BackendUAS/application/views/_partial/sidebar.php
@@ -0,0 +1,18 @@
+<aside class="d-flex flex-column justify-content-evenly bg-info text-center " style="width: 15%; height: 90vh;">
+    <a href="#" class="text-white nav-link active">Dashboard</a>
+    <a href="#" class="text-white nav-link ">Balance</a>
+    <div class="transaksi">
+        <h3>Transaksi</h3>
+        <hr>
+        <a href="#" class="nav-link text-white">Top Up</a>
+        <a href="#" class="nav-link text-white">Pengeluaran</a>
+        <a href="#" class="nav-link text-white">Remainder</a>
+
+    </div>
+    
+    <div class="report">
+        <h3>Report</h3>
+        <hr>
+        <a href="#" class="nav-link text-white">Cetak laporan</a>
+    </div>
+</aside>
