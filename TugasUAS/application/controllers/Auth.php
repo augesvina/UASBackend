17496ed856357ba60c30f78075603e94a02513d7
aaugesvina <89875584+augesvina@users.noreply.github.com>
2021-12-23 21:06:25 +0700

    uas
    uas

---
+++ TugasUAS/application/controllers/Auth.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();  //memanggil method constructor yang ada di CI_controller
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'SukaCita';
+            $this->load->view('templates_login/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates_login/auth_footer');
+        } else { //validasi ketika sukses
+            $this->_login(); //method login private tidak bisa diakses melalui url dengan memberi tanta _
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            //user ada, cek password
+            if (password_verify($password, $user['password'])) {
+                $data = [
+                    'email' => $user['email']
+                ];
+                $this->session->set_userdata($data);
+                redirect('user');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Salah!</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email belum terdaftar!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'Email sudah ada!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[4]|matches[password2]', [
+            'matches' => 'password dont match!',
+            'min_length' => 'password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'SukaCita';
+            $this->load->view('templates_login/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates_login/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Registrasi Berhasil! Silahkan Login!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Logout Berhasil!</div>');
+        redirect('auth');
+    }
+}
