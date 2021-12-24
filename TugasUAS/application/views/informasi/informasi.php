9845f18e77426fc766f1c5f86a5776c5a1862220
aaugesvina <89875584+augesvina@users.noreply.github.com>
2021-12-24 07:51:17 +0700

    UAS

--- TugasUAS/application/views/informasi/informasi.php
+++ TugasUAS/application/views/informasi/informasi.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <head>
     <meta charset="utf-8">
-    <title>Welcome to CodeIgniter</title>
+    <title>Input Buku</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="<?php echo base_url('assets/'); ?>ckeditor/ckeditor.js"></script>
 </head>
